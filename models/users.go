bcbeaeeccc50f218a430afa6769e0c3e317751ac
haewon <n2203020@N2203020ui-MacBookPro.local>
2022-05-20 13:12:05 +0900

    Add vendor

--- models/users.go
+++ models/users.go
@@ -6,13 +6,6 @@ import "github.com/jinzhu/gorm"
 type Users struct {
 	gorm.Model
 	ID       uint64 `json:"id" gorm:"primaryKey"`
-	Email    string `json:"email" gorm:"primaryKey"`
+	Email    string `json:"email"`
 	Password string `json:"password"`
-	UserRole string `json:"user_role"`
-}
-
-type UserRole struct {
-	ID     uint64 `json:"id" gorm:"primaryKey"`
-	UserId uint64 `json:"user_id"`
-	Role   string `json:"role"`
 }
